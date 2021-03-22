d2f349bba6285b4352d9cfe17d5841d20ffe2c69
mesolly <saurav.kandari.123@gmail.com>
2021-03-22 21:50:34 +0530

    heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express ();
 
@@ -47,4 +48,6 @@ app.get('/bad',(request,response)=>{
     })
 });
 
-app.listen(3000);
+app.listen(port,()=>{
+    console.log(`Server is on port:${port}`);
+});
